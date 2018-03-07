2f86f3d0dffbdb6802ab18e4e5f04553a0bc670e
Arie Vainstein <arie@ciphyrus.com>
2018-03-07 22:37:34 +0200

    Add splash screen

---
+++ app/src/main/java/com/taped/taped/SplashActivity.java
@@ -0,0 +1,38 @@
+package com.taped.taped;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private final int STR_SPLASH_TIME = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        startSplashTimer();
+    }
+
+    private void startSplashTimer() {
+        try {
+            Timer timer = new Timer();
+            timer.schedule(new TimerTask() {
+
+                @Override
+                public void run() {
+                    Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }, STR_SPLASH_TIME);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
